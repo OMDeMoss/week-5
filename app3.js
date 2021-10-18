932c4318fc6df2f8320742f595adb099089eb665
Olivia DeMoss <odemoss@goodwillworks.org>
2021-10-18 14:08:38 -0400

    update

--- app3.js
+++ app3.js
@@ -1,13 +1,36 @@
 // 2) Using a for loop print out 1 - 10 to the console
 
+for (var i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
 // 3) Write a while loop with true as the conditional WARNING: 
-This will cause an infinite loop condition so be mindful that you don't save your file if you 're using live server, otherwise don't execute your JS until you complete step 5
+//This will cause an infinite loop condition so be mindful that you don't save your file if you 're using live server, 
+//otherwise don't execute your JS until you complete step 5
+
+while(true){
+    console.log('hello world!');
+    break;
+}
 
 // 4) In the code block of your while loop write a break statement to terminate the loop NOTE: it is ok to save or execute your JavaScript now
 
 // 5) Write a for loop that starts at 99 and DECREMENTS down to 1
 
-// 6) In the code block of the for loop console log the concatenation of your iterator (typically "i") and " bottles of beer on the wall. Take one down pass it around." NOTE: should write "99 bottles of beer on the wall. Take one down pass it around." => "1 bottles of beer on the wall. Take one down pass it around." BONUS: Format it to where it is "1 bottle" instead of "1 bottles"
+for (var i = 99; i >=1; i--) {
+    if (i !==1)
+    console.log(i + "bottles of beer on the wall. Take one down pass it around.");
+
+    if (i === 1)
+}
+
+// 6) In the code block of the for loop console log the concatenation of your iterator 
+//(typically "i") and " bottles of beer on the wall. Take one down pass it around." 
+//NOTE: should write "99 bottles of beer on the wall. Take one down pass it around." 
+//=> "1 bottles of beer on the wall. Take one down pass it around." 
+//BONUS: Format it to where it is "1 bottle" instead of "1 bottles"
+
+
 
 // 7) Create a variable named bools set it to the array [true, true, false, true, false, true, true, true]
 
